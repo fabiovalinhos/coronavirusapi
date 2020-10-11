fb543fa1584cd80ca7bb87b9e20dd179edddd953
fabiovalinhos <fabiovalinhos@gmail.com>
2020-10-11 11:58:28 -0300

    Put RefreshIndicator when pull screen  - get data when make it

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,4 +1,8 @@
+import 'package:coronavirusrestapiflutter/app/repositories/data_repository.dart';
+import 'package:coronavirusrestapiflutter/app/services/api.dart';
+import 'package:coronavirusrestapiflutter/app/ui/endpoint_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -6,8 +10,37 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _cases;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateData();
+  }
+
+  Future<void> _updateData() async {
+    final dataRepository = Provider.of<DataRepository>(context, listen: false);
+    final cases = await dataRepository.getEndpointData(EndPoint.cases);
+    setState(() => _cases = cases);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Coronavirus tracker'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: _updateData,
+        child: ListView(
+          children: [
+            EndpointCard(
+              endpoint: EndPoint.cases,
+              value: _cases,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
